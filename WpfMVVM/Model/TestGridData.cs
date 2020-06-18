f8a7314cb48ac3755f2b9271b261bb30ea23c206
kevan.chen <kevan.chen@sinopa.com>
2020-06-18 13:54:48 +0800

    加入grid樣本 加入radioBtn樣本

---
+++ WpfMVVM/Model/TestGridData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMVVM.Model
+{
+    public class TestGridData
+    {
+        public string AColumn { get; set; }
+        public string BColumn { get; set; }
+        public string CColumn { get; set; }
+    }
+}
--- WpfMVVM/ViewModel/MainWindowViewModel.cs
+++ WpfMVVM/ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using WpfMVVM.CommonUtility;
+using WpfMVVM.Model;
 
 namespace WpfMVVM.ViewModel
 {
@@ -23,6 +25,8 @@ namespace WpfMVVM.ViewModel
         }
 
         #endregion
+
+        #region 屬性
 
         private string _labText = "LabTest";
 
@@ -52,18 +56,118 @@ namespace WpfMVVM.ViewModel
                 this.NotifyPropertyChanged("TbText");
             }
         }
+
+        private bool _testRbtn = true;
+
+        /// <summary>
+        /// RadioBtn的變數
+        /// </summary>
+        public bool TestRbtn
+        {
+            get { return _testRbtn; }
+            set
+            {
+                _testRbtn = value;
+
+                //當值改變時會觸發事件
+                this.NotifyPropertyChanged("TestRbtn");
+            }
+        }
+
+        private bool _testRbtn2 { get; set; }
+
+        /// <summary>
+        /// RadioBtn的變數
+        /// </summary>
+        public bool TestRbtn2
+        {
+            get { return _testRbtn2; }
+            set
+            {
+                _testRbtn2 = value;
+
+                //當值改變時會觸發事件
+                this.NotifyPropertyChanged("TestRbtn2");
+            }
+        }
+
+        /// <summary>
+        /// 選擇時所回傳資料的私有變數
+        /// </summary>
+        private TestGridData _selectedValue = null;
+
+        /// <summary>
+        /// 選擇時所回傳資料的變數
+        /// </summary>
+        public TestGridData SelectedValue
+        {
+            get { return _selectedValue; }
+            set
+            {
+                _selectedValue = value;
+
+                //當值改變時會觸發事件
+                this.NotifyPropertyChanged("SelectedValue");
+            }
+        }
+
+        /// <summary>
+        /// 存取所有查尋回來資料用的集合
+        /// </summary>
+        private List<TestGridData> _dataList = new List<TestGridData>();
+
+        /// <summary>
+        /// 存取DataGride顯示用的資料
+        /// </summary>
+        private ObservableCollection<TestGridData> _dataListView = new ObservableCollection<TestGridData>();
+
+        /// <summary>
+        /// Binding顯示用資料的變數
+        /// </summary>
+        public ObservableCollection<TestGridData> DataData
+        {
+            get
+            {
+                if (_dataListView.Count == 0)
+                    _dataListView = GetDataList(); //may call web API ..WCF
+
+                return _dataListView;
+            }
+            set { _dataListView = value; }
+        }
+
+        #endregion
+
+        private ObservableCollection<TestGridData> GetDataList()
+        {
+            List<TestGridData> list = new List<TestGridData>()
+            {
+                new TestGridData(){ AColumn = "Q" , BColumn = "W" , CColumn = "E"},
+                new TestGridData(){ AColumn = "A" , BColumn = "S" , CColumn = "D"},
+                new TestGridData(){ AColumn = "Z" , BColumn = "X" , CColumn = "C"},
+                new TestGridData(){ AColumn = "R" , BColumn = "T" , CColumn = "Y"},
+                new TestGridData(){ AColumn = "F" , BColumn = "G" , CColumn = "H"},
+            };
+            foreach (var item in list)
+            {
+                var tmp = new TestGridData() { };
+                tmp.AColumn = item.AColumn;
+                tmp.BColumn = item.BColumn;
+                tmp.CColumn = item.CColumn;
+
+                _dataListView.Add(tmp);
+            }
+            return _dataListView;
+        }
+
+        #region 事件
         public ICommand BtnTestCommand
         {
             get { return new RelayCommand(BtnTestCommandExecute, CanBtnTestCommand); }
         }
 
         /// <summary>
-        /// 寫入圖檔用的物件
-        /// </summary>
-        List<FileData> _fd = new List<FileData>();
-
-        /// <summary>
-        /// 觸發OpenFile事件的方法
+        /// 觸發BtnTest事件的方法
         /// </summary>
         private void BtnTestCommandExecute()
         {
@@ -82,5 +186,27 @@ namespace WpfMVVM.ViewModel
         {
             return true;
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICommand ShowDataCommand
+        {
+            get { return new RelayCommand(ShowDataCommandExecute, CanShowDataCommand); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void ShowDataCommandExecute()
+        {
+            MessageBox.Show(SelectedValue.AColumn);
+        }
+
+        private bool CanShowDataCommand()
+        {
+            return true;
+        }
+        #endregion
     }
 }
